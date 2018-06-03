782a80705e55dcbd8770230417330275a6db0a13
Lynn <Lynn@LAPTOP-635O7HE9>
2018-06-03 14:24:16 +0800

    Jenkins Demonstration

---
+++ src/test/java/com/bbc/JenkinsDemo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.bbc.JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld 
+{
+	@Test
+	public void helloWorld()
+	{
+		System.out.println("Hello World!");
+	}
+
+}
